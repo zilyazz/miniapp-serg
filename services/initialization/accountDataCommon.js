eefe8f9efd892d9430fdd3ef5f2e07e610c4e1e1
zilyazz <ilya.erygin.01@gmail.com>
2026-06-04 22:09:23 +0400

    обратно

--- services/initialization/accountDataCommon.js
+++ services/initialization/accountDataCommon.js
@@ -54,7 +54,7 @@ async function finalizeAccountData(row, equipMap, internalUserKey) {
   return {
     userId,
     score_crystal: row.score_crystal || 0,
-    newUserCreated: true,//!!row.is_new,
+    newUserCreated: !!row.is_new,
     subscriptionExpired,
     sound: row.sound || 1,
     allow: true,
